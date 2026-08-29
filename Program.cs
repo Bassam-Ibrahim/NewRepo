023e0ae9cf2edde71e3f1fe26acdbcc1747f520e
Bassam Ibrahim Abu El-Saud <bassamb672005@gmail.com>
2026-08-29 22:45:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//questionNumber-1
+class Book
+{
+
+    public string title;
+    public int pages;
+
+}
+class Program
+{
+    static void Main()
+    {
+
+        Book book = new Book();
+        book.title = "english";
+        book.pages = 10;
+        Object obj = book;
+        Console.WriteLine(obj);
+    }
+}
